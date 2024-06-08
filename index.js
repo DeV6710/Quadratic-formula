6bb8e9ae3c5acdbeaaa30815e80188d2345c2446
youssef khir <92634788+DeV6710@users.noreply.github.com>
2024-06-08 17:26:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ let back = document.getElementById("back")
 let bruh = document.getElementById("p")
 
 
+
+
 sub.addEventListener("click",function(e){
     e.preventDefault()
     let av = parseInt(a.value)
@@ -42,7 +44,19 @@ radd.addEventListener("click",function(e){
     let cv = parseInt(c.value)
     let rad = (bv ** 2) - 4*(av)*(cv)
     let double_a = 2 *av
-    equal.innerText = "your values are "+ -bv +" ± "+"√"+rad+ "  / " +double_a   
+    let test = Math.sqrt(rad)
+    if(Number.isInteger(test) === true){
+        Math.sqrt(rad)
+        console.log(test)
+        console.log(rad *rad)
+        equal.innerText = "your values are "+ -bv +" ± "+test+ "  / " +double_a
+    }else{
+        equal.innerText = "your values are "+ -bv +" ± "+"√"+rad+ "  / " +double_a
+        console.log(test)
+        console.log(rad *rad)
+    }
+    
+    
     
     steps.style.display = "inline-block"
     
@@ -55,6 +69,7 @@ steps.addEventListener("click",function(e){
 
     let rad = (bv ** 2) - 4*(av)*(cv)
     let double_a = 2 *av
+    let test = Math.sqrt(rad)
 
     let umb = Math.sqrt((bv ** 2) - 4*(av)*(cv))
     let neg = (-bv - umb) / (2 * av);
@@ -71,9 +86,14 @@ steps.addEventListener("click",function(e){
     bruh.style.display = "none"
 
     first.innerText = "1. "+av +"x² +"+bv+"+"+cv
+   
     second.innerText = "2. "+-bv +" ± "+"√ "+ bv+"²  -4"+ " * "+av+" * "+cv+ " / "+"2 * "+av
-    third.innerText = "3. "+"your values are "+ -bv +" ± "+"√"+rad+ "  / " +double_a +"    and " + "( " +[(pos).toFixed(3),(neg).toFixed(3)] + " )"
-
+    third.innerText = "3. "+"your values are "+ -bv +" ± "+"√"+rad+ "  / " +double_a +"    or " + "( " +[(pos).toFixed(3),(neg).toFixed(3)] + " )"
+    if(Number.isInteger(test) === true){
+        third.innerText = "3. "+"your values are "+ -bv +" ± "+test+ "  / " +double_a +"    or " + "( " +[(pos).toFixed(3),(neg).toFixed(3)] + " )"
+    }else{
+        third.innerText = "3. "+"your values are "+ -bv +" ± "+"√"+rad+ "  / " +double_a +"    or " + "( " +[(pos).toFixed(3),(neg).toFixed(3)] + " )"
+    }
     back.style.display = "inline-block"
     back.addEventListener("click",function(){
         location.reload();
